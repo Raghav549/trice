03b526460deb901085ca67fddebc31ec9e47a87a
zyra noxe <raghavkashyap2229@gmail.com>
2026-09-14 10:07:06 +0530

    release: expose integrated ComputationalBody API

--- trice/__init__.py
+++ trice/__init__.py
@@ -1,3 +1,7 @@
 """TRICE whole-organism computational architecture."""
 
-__version__ = "0.1.0"
+from .body import ComputationalBody
+
+__version__ = "0.2.0"
+
+__all__ = ["ComputationalBody", "__version__"]
