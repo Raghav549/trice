64dcf5df6f1ac22cdb380c12f28029940dcc3095
zyra noxe <raghavkashyap2229@gmail.com>
2026-09-14 09:30:26 +0530

    feat: export whole-body coverage catalog

---
+++ trice/coverage/__init__.py
@@ -0,0 +1,5 @@
+"""Coverage tracking for the TRICE whole-body model."""
+
+from .catalog import BiologicalComponent, CoverageStatus, MINIMUM_CATALOG, missing_components
+
+__all__ = ["BiologicalComponent", "CoverageStatus", "MINIMUM_CATALOG", "missing_components"]
