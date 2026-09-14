aeb86b6bcd3584639438447c4aa52784b1b0ff14
zyra noxe <raghavkashyap2229@gmail.com>
2026-09-14 09:29:53 +0530

    feat: export initial TRICE subsystems

---
+++ trice/modules/__init__.py
@@ -0,0 +1,8 @@
+"""TRICE body-inspired subsystems."""
+
+from .endocrine import EndocrineModule
+from .immune import ImmuneModule
+from .memory import MemoryModule
+from .neural import NeuralModule
+
+__all__ = ["EndocrineModule", "ImmuneModule", "MemoryModule", "NeuralModule"]
