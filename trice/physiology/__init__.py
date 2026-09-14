c85d2fdbfb3e16d0754eec672f9ec954d7b3b219
zyra noxe <raghavkashyap2229@gmail.com>
2026-09-14 10:06:50 +0530

    refactor(physiology): expose integrated physiology models

--- trice/physiology/__init__.py
+++ trice/physiology/__init__.py
@@ -1,17 +1,27 @@
 """Multi-scale physiology abstractions used by TRICE."""
 
+from .blood import BloodModel
 from .cardiovascular import CardiovascularModel
-from .respiratory import RespiratoryModel
-from .renal import RenalModel
+from .cellular import CellularModel
+from .digestion import DigestionModel
+from .integumentary import IntegumentaryModel
+from .lymphatic import LymphaticModel
 from .metabolism import MetabolismModel
 from .motor import MotorModel
+from .renal import RenalModel
+from .respiratory import RespiratoryModel
 from .sensory import SensoryModel
 
 __all__ = [
+    "BloodModel",
     "CardiovascularModel",
-    "RespiratoryModel",
-    "RenalModel",
+    "CellularModel",
+    "DigestionModel",
+    "IntegumentaryModel",
+    "LymphaticModel",
     "MetabolismModel",
     "MotorModel",
+    "RenalModel",
+    "RespiratoryModel",
     "SensoryModel",
 ]
