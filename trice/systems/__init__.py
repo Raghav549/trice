c3bca2f087ea700618381054cd3c966a88f798ab
zyra noxe <raghavkashyap2229@gmail.com>
2026-09-14 09:34:57 +0530

    systems: export whole-body registry

---
+++ trice/systems/__init__.py
@@ -0,0 +1,5 @@
+"""Whole-body system registries."""
+
+from .whole_body import BiologicalComponent, CoverageStatus, COMPONENTS, coverage_by_status, coverage_map
+
+__all__ = ["BiologicalComponent", "CoverageStatus", "COMPONENTS", "coverage_by_status", "coverage_map"]
