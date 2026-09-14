300ba640a7782d5dce63c201e5207bd1e21acb0b
zyra noxe <raghavkashyap2229@gmail.com>
2026-09-14 10:12:07 +0530

    Expose deterministic benchmark API from research package

--- trice/research/__init__.py
+++ trice/research/__init__.py
@@ -1,6 +1,15 @@
 """Research operators and reproducible experimentation utilities."""
 
+from .benchmark import BenchmarkResult, run as run_benchmark
 from .evolution import EvolutionConfig, EvolutionEngine, Genome
 from .objectives import Objective, multi_objective_score
 
-__all__ = ["EvolutionConfig", "EvolutionEngine", "Genome", "Objective", "multi_objective_score"]
+__all__ = [
+    "BenchmarkResult",
+    "EvolutionConfig",
+    "EvolutionEngine",
+    "Genome",
+    "Objective",
+    "multi_objective_score",
+    "run_benchmark",
+]
