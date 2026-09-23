39acc0f0d628fce716f9a764997f8ca771dfdc9b
zyra noxe <raghavkashyap2229@gmail.com>
2026-09-24 02:37:27 +0530

    feat: build human layer __init__.py

---
+++ trice/human/__init__.py
@@ -0,0 +1,5 @@
+"""Whole-human computational domain model namespace.
+
+TRICE models human biology as explicit computational abstractions. It does not
+claim exact biological equivalence or a solved model of subjective experience.
+"""
